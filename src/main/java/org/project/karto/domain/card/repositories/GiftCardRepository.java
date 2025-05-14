72fbd9c662a49943b959bdaf37354419e9b5598f
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-05-14 19:42:48 +0400

    Fix repositories

--- src/main/java/org/project/karto/domain/card/repositories/GiftCardRepository.java
+++ src/main/java/org/project/karto/domain/card/repositories/GiftCardRepository.java
@@ -1,10 +1,10 @@
 package org.project.karto.domain.card.repositories;
 
 import org.project.karto.domain.card.entities.GiftCard;
+import org.project.karto.domain.card.enumerations.Store;
 import org.project.karto.domain.card.value_objects.BuyerID;
 import org.project.karto.domain.card.value_objects.CardID;
 import org.project.karto.domain.card.value_objects.OwnerID;
-import org.project.karto.domain.card.value_objects.StoreID;
 import org.project.karto.domain.common.containers.Result;
 
 import java.util.List;
@@ -21,5 +21,5 @@ public interface GiftCardRepository {
 
     Result<List<GiftCard>, Throwable> findBy(OwnerID ownerID);
 
-    Result<List<GiftCard>, Throwable> findBy(StoreID storeID);
+    Result<List<GiftCard>, Throwable> findBy(Store store);
 }
