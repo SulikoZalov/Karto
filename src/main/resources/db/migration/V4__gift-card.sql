5c07f06b5dd9c94b59bfd3f108fd47fd859a10cf
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-05-27 03:14:24 +0400

    SQL migrations for new version of gift card

--- src/main/resources/db/migration/V4__gift-card.sql
+++ src/main/resources/db/migration/V4__gift-card.sql
@@ -6,10 +6,12 @@ CREATE TABLE gift_card (
     gift_card_status VARCHAR(7) NOT NULL CHECK (gift_card_status IN ('PENDING', 'ACTIVE', 'EXPIRED', 'USED_UP'))
     balance BIGINT NOT NULL,
     count_of_uses SMALLINT NOT NULL,
+    max_count_of_uses SMALLINT NOT NULL,
     is_verified BOOLEAN NOT NULL,
     secret_key VARCHAR NOT NULL,
     counter BIGINT NOT NULL,
     creation_date TIMESTAMP NOT NULL,
     expiration_date TIMESTAMP NOT NULL,
+    last_usage TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 );
