8641b1ca2a758a1319f0f96c1e1df7c2b5dc9c27
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-06-02 02:35:07 +0400

    Size validation for registration number

--- src/main/java/org/project/karto/domain/companies/value_objects/RegistrationNumber.java
+++ src/main/java/org/project/karto/domain/companies/value_objects/RegistrationNumber.java
@@ -7,6 +7,9 @@ import java.util.regex.Pattern;
 
 public record RegistrationNumber(String countryCode, String value) {
     private static final Set<String> COUNTRY_CODES = Set.of(Locale.getISOCountries());
+    private static final int COUNTRY_CODE_LENGTH = 2;
+    private static final int REG_NUMBER_MIN_LENGTH = 5;
+    private static final int REG_NUMBER_MAX_LENGTH = 20;
     private static final String REGISTRATION_NUMBER_REGEX = "[A-Z0-9\\-\\s]{5,20}";
     private static final Pattern REGISTRATION_NUMBER_PATTERN = Pattern.compile(REGISTRATION_NUMBER_REGEX);
 
@@ -14,13 +17,20 @@ public record RegistrationNumber(String countryCode, String value) {
         if (countryCode == null || countryCode.isBlank())
             throw new IllegalArgumentException("Country code must be provided.");
 
-        if (value == null || value.isBlank())
-            throw new IllegalArgumentException("Registration number must be provided.");
+        if (countryCode.length() == COUNTRY_CODE_LENGTH)
+            throw new IllegalArgumentException("Country code must be 2 letters.");
 
         if (!COUNTRY_CODES.contains(countryCode))
             throw new IllegalArgumentException("This country code do not exists.");
 
-        Matcher matcher = REGISTRATION_NUMBER_PATTERN.matcher(value.trim());
+        if (value == null || value.isBlank())
+            throw new IllegalArgumentException("Registration number must be provided.");
+
+        String trimmed = value.trim();
+        if (trimmed.length() < REG_NUMBER_MIN_LENGTH || trimmed.length() > REG_NUMBER_MAX_LENGTH)
+            throw new IllegalArgumentException("Registration number must be 5 to 20 characters long.");
+
+        Matcher matcher = REGISTRATION_NUMBER_PATTERN.matcher(trimmed);
         if (!matcher.matches())
             throw new IllegalArgumentException("Invalid registration number format.");
     }
