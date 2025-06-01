888af23277840ac7974e00ca6fe0bf0ca45dba19
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-06-02 03:01:35 +0400

    SQL migration for company aggregate

---
+++ src/main/resources/db/migration/V6__companies.sql
@@ -0,0 +1,13 @@
+CREATE TABLE companies (
+    id CHAR(36) NOT NULL,
+    state_code CHAR(2) NOT NULL,
+    registration_number VARCHAR(20) NOT NULL,
+    company_name VARCHAR(255) NOT NULL,
+    email VARCHAR NOT NULL,
+    phone VARCHAR NOT NULL,
+    password VARCHAR(64) NOT NULL,
+    expiration_period_days INTEGER NOT NULL,
+    max_usage_count INTEGER NOT NULL,
+    creation_date TIMESTAMP NOT NULL,
+    last_updated TIMESTAMP NOT NULL
+)
