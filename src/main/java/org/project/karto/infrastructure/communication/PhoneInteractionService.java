f909cc6bbdd1fe179c39c8f6d65537d85acde6cc
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-06-08 19:51:45 +0400

    additional function for PhoneInteractionService

--- src/main/java/org/project/karto/infrastructure/communication/PhoneInteractionService.java
+++ src/main/java/org/project/karto/infrastructure/communication/PhoneInteractionService.java
@@ -3,6 +3,7 @@ package org.project.karto.infrastructure.communication;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.project.karto.domain.companies.entities.PartnerVerificationOTP;
 import org.project.karto.domain.user.entities.OTP;
 import org.project.karto.domain.common.value_objects.Phone;
 
@@ -12,6 +13,10 @@ public class PhoneInteractionService {
     private static final String KARTO_PHONE = "+15005550006";
 
     public void sendOTP(Phone phone, OTP otp) {
+        Message.creator(new PhoneNumber(phone.phoneNumber()), new PhoneNumber(KARTO_PHONE), otp.otp()).create();
+    }
+
+    public void sendOTP(Phone phone, PartnerVerificationOTP otp) {
         Message.creator(new PhoneNumber(phone.phoneNumber()), new PhoneNumber(KARTO_PHONE), otp.otp()).create();
     }
 
