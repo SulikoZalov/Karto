e7785ebac2d04f1ac2e6148dbe7a313c807557a5
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-06-13 01:16:58 +0400

    Feat better error messages

---
+++ src/main/java/org/project/karto/application/dto/common/ErrorMessage.java
@@ -0,0 +1,3 @@
+package org.project.karto.application.dto.common;
+
+public record ErrorMessage(String errorMessage) {}
--- src/main/java/org/project/karto/application/util/RestUtil.java
+++ src/main/java/org/project/karto/application/util/RestUtil.java
@@ -3,13 +3,16 @@ package org.project.karto.application.util;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.project.karto.application.dto.common.ErrorMessage;
 
 public class RestUtil {
+
+    private RestUtil() {}
 
     public static WebApplicationException responseException(Response.Status status, String message) {
         return new WebApplicationException(Response
                 .status(status)
-                .entity(message)
+                .entity(new ErrorMessage(message))
                 .type(MediaType.APPLICATION_JSON_TYPE)
                 .build());
     }
