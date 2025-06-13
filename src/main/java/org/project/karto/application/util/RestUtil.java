53fee8f631b9f7b724c232306c0e0fe2bd2c4dc5
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-06-13 18:31:05 +0400

    Better error messages

--- src/main/java/org/project/karto/application/util/RestUtil.java
+++ src/main/java/org/project/karto/application/util/RestUtil.java
@@ -1,5 +1,7 @@
 package org.project.karto.application.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -7,12 +9,21 @@ import org.project.karto.application.dto.common.ErrorMessage;
 
 public class RestUtil {
 
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     private RestUtil() {}
 
     public static WebApplicationException responseException(Response.Status status, String message) {
+        String entity;
+        try {
+            entity = objectMapper.writeValueAsString(new ErrorMessage(message));
+        } catch (JsonProcessingException e) {
+            throw new IllegalStateException("Unexpected exception, error message can`t be serialized.");
+        }
+
         return new WebApplicationException(Response
                 .status(status)
-                .entity(new ErrorMessage(message))
+                .entity(entity)
                 .type(MediaType.APPLICATION_JSON_TYPE)
                 .build());
     }
