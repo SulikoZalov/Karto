732edf4011187b308245d48313c09438928ed913
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-07-01 19:35:42 +0400

    Feat flyway migration for payment intent entity

---
+++ src/main/resources/db/migration/V9_payment_intent.sql
@@ -0,0 +1,12 @@
+CREATE TABLE payment_intent (
+    id CHAR(36) NOT NULL,
+    buyer_id CHAR(36) NOT NULL,
+    card_id CHAR(36) NOT NULL,
+    store_id CHAR(36),
+    order_id BIGINT NOT NULL,
+    total_amount NUMERIC NOT NULL,
+    creation_date TIMESTAMP NOT NULL,
+    result_date TIMESTAMP,
+    status VARCHAR(7) NOT NULL CHECK (status IN ('PENDING', 'SUCCESS', 'CANCEL', 'FAILURE')),
+    PRIMARY KEY (id)
+);
