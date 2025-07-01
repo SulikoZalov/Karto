e347d7eebd89ef961c953560dccf6519cd05d1cb
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-07-02 02:37:16 +0400

    Refactor sql migration

--- src/main/resources/db/migration/V9__payment_intent.sql
+++ src/main/resources/db/migration/V9__payment_intent.sql
@@ -8,5 +8,6 @@ CREATE TABLE payment_intent (
     creation_date TIMESTAMP NOT NULL,
     result_date TIMESTAMP,
     status VARCHAR(7) NOT NULL CHECK (status IN ('PENDING', 'SUCCESS', 'CANCEL', 'FAILURE')),
+    is_confirmed BOOLEAN NOT NULL,
     PRIMARY KEY (id)
 );
