8716fe3e1a6de8658553b97755c6990cea8baa65
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-07-04 22:43:58 +0400

    Feat additional test for check repository

--- src/test/groovy/org/project/karto/unit/repository/CheckRepositoryTest.groovy
+++ src/test/groovy/org/project/karto/unit/repository/CheckRepositoryTest.groovy
@@ -9,10 +9,14 @@ import org.project.karto.domain.card.enumerations.PaymentType
 import org.project.karto.domain.card.value_objects.*
 import org.project.karto.domain.common.value_objects.Amount
 import org.project.karto.infrastructure.repository.JDBCCheckRepository
+import org.project.karto.infrastructure.repository.JDBCGiftCardRepository
 import org.project.karto.util.PostgresTestResource
 import org.project.karto.util.TestDataGenerator
 import spock.lang.Specification
+import spock.lang.Unroll
 
+import java.math.RoundingMode
+import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
 
 import static org.project.karto.util.TestDataGenerator.orderID
@@ -24,6 +28,9 @@ class CheckRepositoryTest extends Specification {
 
     @Inject
     JDBCCheckRepository repo
+
+    @Inject
+    JDBCGiftCardRepository giftCardRepo
 
     @Inject
     Util util
@@ -42,6 +49,70 @@ class CheckRepositoryTest extends Specification {
         notThrown(Exception)
         result.success()
         result.orElseThrow() > 0
+    }
+
+    @Unroll
+    def "successful save pf payment check [#index]"() {
+        given: "Saved gift card"
+        def giftCard = TestDataGenerator.generateSelfBougthGiftCard(
+                util.generateActivateAndSaveUser(),
+                util.generateActivateAndSaveCompany()
+        )
+        def saveResult = giftCardRepo.save(giftCard)
+
+        expect: "Initial save should succeed"
+        saveResult.success()
+        saveResult.value() == 1
+
+        when: "Activating the card"
+        giftCard.activate()
+        def activationResult = giftCardRepo.update(giftCard)
+
+        then: "Activation update should succeed"
+        activationResult.success()
+        activationResult.value() == 1
+
+        when: "Performing a transaction"
+        def reloadedCard1 = giftCardRepo.findBy(giftCard.id()).value()
+        def amount = new Amount(reloadedCard1.balance().value().divide(BigDecimal.valueOf(10), RoundingMode.HALF_UP))
+        def paymentIntent = reloadedCard1.initializeTransaction(amount, orderID())
+        paymentIntent.markAsSuccess(new ExternalPayeeDescription("desc"))
+        def giftCardTransactionInitializationUpdateRes = giftCardRepo.update(reloadedCard1)
+
+        then: "Expect successful transaction initialization update"
+        giftCardTransactionInitializationUpdateRes.success()
+        giftCardTransactionInitializationUpdateRes.value() == 1
+
+        when : "Applying transaction and update"
+        def reloadedCard2 = giftCardRepo.findBy(reloadedCard1.id()).value()
+        def check = reloadedCard2.applyTransaction(
+                paymentIntent,
+                new UserActivitySnapshot(
+                        reloadedCard2.ownerID().get().value(),
+                        new BigDecimal("5000"),
+                        60,
+                        LocalDateTime.now(),
+                        10
+                ),
+                Currency.getInstance("USD"),
+                PaymentType.KARTO_PAYMENT,
+                new PaymentSystem("UP")
+        )
+        def transactionResult = giftCardRepo.update(reloadedCard2)
+
+        then: "Transaction update should succeed"
+        transactionResult.success()
+        transactionResult.value() == 1
+
+        when: "Save check"
+        def resultOfCheckSaving = repo.save(check)
+
+        then: "Expect successful check saving"
+        resultOfCheckSaving.success()
+        resultOfCheckSaving.value() == 1
+
+        where:
+        index << (1..10)
     }
 
     void "fail saving twice"() {
