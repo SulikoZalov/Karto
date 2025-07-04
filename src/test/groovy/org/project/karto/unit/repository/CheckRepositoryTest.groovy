ae0efc74db9874c60e016ccbd40d75c636213463
Ain Grace <qnicat32@gmail.com>
2025-07-04 14:59:49 +0400

    tests for check repository

---
+++ src/test/groovy/org/project/karto/unit/repository/CheckRepositoryTest.groovy
@@ -0,0 +1,199 @@
+package org.project.karto.unit.repository
+
+import com.aingrace.test.spock.QuarkusSpockTest
+import io.quarkus.test.common.QuarkusTestResource
+import jakarta.enterprise.context.Dependent
+import jakarta.inject.Inject
+import org.project.karto.domain.card.entities.CardPurchaseIntent
+import org.project.karto.domain.card.enumerations.PaymentType
+import org.project.karto.domain.card.value_objects.BuyerID
+import org.project.karto.domain.card.value_objects.Currency
+import org.project.karto.domain.card.value_objects.ExternalPayeeDescription
+import org.project.karto.domain.card.value_objects.PaymentSystem
+import org.project.karto.domain.card.value_objects.StoreID
+import org.project.karto.domain.common.value_objects.Amount
+import org.project.karto.infrastructure.repository.JDBCCheckRepository
+import org.project.karto.util.PostgresTestResource
+import org.project.karto.util.TestDataGenerator
+import spock.lang.Specification
+
+import java.time.temporal.ChronoUnit
+
+
+@Dependent
+@QuarkusSpockTest
+@QuarkusTestResource(value = PostgresTestResource.class)
+class CheckRepositoryTest extends Specification{
+
+    @Inject
+    JDBCCheckRepository repo
+
+    void "successful save"() {
+        given:
+        def intent = CardPurchaseIntent.of(UUID.randomUUID(), new BuyerID(UUID.randomUUID()), null, 1L, new Amount(100))
+        def fee = TestDataGenerator.generateFee(BigDecimal.valueOf(0.05))
+        def check = intent.markAsSuccess(fee, new Currency("AZN"), PaymentType.FOREIGN_BANK, new PaymentSystem("UP"), new ExternalPayeeDescription("desc"))
+
+        when:
+        def result = repo.save(check)
+
+        then:
+        notThrown(Exception)
+        result.success()
+        result.orElseThrow() > 0
+    }
+
+    void "fail saving twice"() {
+        given:
+        def intent = CardPurchaseIntent.of(UUID.randomUUID(), new BuyerID(UUID.randomUUID()), null, 1L, new Amount(100))
+        def fee = TestDataGenerator.generateFee(BigDecimal.valueOf(0.05))
+        def check = intent.markAsSuccess(fee, new Currency("AZN"), PaymentType.FOREIGN_BANK, new PaymentSystem("UP"), new ExternalPayeeDescription("desc"))
+
+        when:
+        def result = repo.save(check)
+        def fail_result = repo.save(check)
+
+        then:
+        notThrown(Exception)
+        result.success()
+        result.orElseThrow() > 0
+
+        and:
+        !fail_result.success()
+    }
+
+    void "successful find by check id"() {
+        given:
+        def intent = CardPurchaseIntent.of(UUID.randomUUID(), new BuyerID(UUID.randomUUID()), null, 1L, new Amount(100))
+        def fee = TestDataGenerator.generateFee(BigDecimal.valueOf(0.05))
+        def check = intent.markAsSuccess(fee, new Currency("AZN"), PaymentType.FOREIGN_BANK, new PaymentSystem("UP"), new ExternalPayeeDescription("desc"))
+
+        when:
+        def result = repo.save(check)
+
+        then:
+        notThrown(Exception)
+        result.success()
+        result.orElseThrow() > 0
+
+        when:
+        def find_result = repo.findBy(check.id())
+
+        then:
+        notThrown(Exception)
+        find_result.success()
+
+        def found_check = find_result.orElseThrow()
+
+        found_check.id() == check.id()
+        found_check.orderID() == check.orderID()
+        found_check.buyerID() == check.buyerID()
+        found_check.totalAmount() == check.totalAmount()
+        found_check.currency() == check.currency()
+        found_check.paymentType() == check.paymentType()
+        found_check.internalFee() == check.internalFee()
+        found_check.externalFee() == check.externalFee()
+        found_check.paymentSystem() == check.paymentSystem()
+        found_check.description() == check.description()
+        found_check.creationDate().truncatedTo(ChronoUnit.MILLIS) == check.creationDate().truncatedTo(ChronoUnit.MILLIS)
+
+        if (found_check.storeID() != null && check.storeID() != null) {
+            found_check.storeID() == check.storeID()
+        }
+
+        if (found_check.cardID() != null && check.cardID() != null) {
+            found_check.cardID() == check.cardID()
+        }
+
+    }
+
+    void "successful find by buyer id"() {
+        given:
+        def intent = CardPurchaseIntent.of(UUID.randomUUID(), new BuyerID(UUID.randomUUID()), null, 1L, new Amount(100))
+        def fee = TestDataGenerator.generateFee(BigDecimal.valueOf(0.05))
+        def check = intent.markAsSuccess(fee, new Currency("AZN"), PaymentType.FOREIGN_BANK, new PaymentSystem("UP"), new ExternalPayeeDescription("desc"))
+
+        when:
+        def result = repo.save(check)
+
+        then:
+        notThrown(Exception)
+        result.success()
+        result.orElseThrow() > 0
+
+        when:
+        def find_result = repo.findBy(check.buyerID())
+
+        then:
+        notThrown(Exception)
+        find_result.success()
+
+        def found_check = find_result.orElseThrow().getFirst()
+
+        found_check.id() == check.id()
+        found_check.orderID() == check.orderID()
+        found_check.buyerID() == check.buyerID()
+        found_check.totalAmount() == check.totalAmount()
+        found_check.currency() == check.currency()
+        found_check.paymentType() == check.paymentType()
+        found_check.internalFee() == check.internalFee()
+        found_check.externalFee() == check.externalFee()
+        found_check.paymentSystem() == check.paymentSystem()
+        found_check.description() == check.description()
+        found_check.creationDate().truncatedTo(ChronoUnit.MILLIS) == check.creationDate().truncatedTo(ChronoUnit.MILLIS)
+
+        if (found_check.storeID() != null && check.storeID() != null) {
+            found_check.storeID() == check.storeID()
+        }
+
+        if (found_check.cardID() != null && check.cardID() != null) {
+            found_check.cardID() == check.cardID()
+        }
+
+    }
+
+    void "successful find by store id"() {
+        given:
+        def intent = CardPurchaseIntent.of(UUID.randomUUID(), new BuyerID(UUID.randomUUID()), new StoreID(UUID.randomUUID()), 1L, new Amount(100))
+        def fee = TestDataGenerator.generateFee(BigDecimal.valueOf(0.05))
+        def check = intent.markAsSuccess(fee, new Currency("AZN"), PaymentType.FOREIGN_BANK, new PaymentSystem("UP"), new ExternalPayeeDescription("desc"))
+
+        when:
+        def result = repo.save(check)
+
+        then:
+        notThrown(Exception)
+        result.success()
+        result.orElseThrow() > 0
+
+        when:
+        def find_result = repo.findBy(check.storeID().orElseThrow())
+
+        then:
+        notThrown(Exception)
+        find_result.success()
+
+        def found_check = find_result.orElseThrow().getFirst()
+
+        found_check.id() == check.id()
+        found_check.orderID() == check.orderID()
+        found_check.buyerID() == check.buyerID()
+        found_check.totalAmount() == check.totalAmount()
+        found_check.currency() == check.currency()
+        found_check.paymentType() == check.paymentType()
+        found_check.internalFee() == check.internalFee()
+        found_check.externalFee() == check.externalFee()
+        found_check.paymentSystem() == check.paymentSystem()
+        found_check.description() == check.description()
+        found_check.creationDate().truncatedTo(ChronoUnit.MILLIS) == check.creationDate().truncatedTo(ChronoUnit.MILLIS)
+
+        if (found_check.storeID() != null && check.storeID() != null) {
+            found_check.storeID() == check.storeID()
+        }
+
+        if (found_check.cardID() != null && check.cardID() != null) {
+            found_check.cardID() == check.cardID()
+        }
+
+    }
+}
