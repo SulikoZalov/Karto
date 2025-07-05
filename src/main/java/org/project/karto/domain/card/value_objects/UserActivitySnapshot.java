33c7cdebc33788b167fb53bd2551af11ddf0973d
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-07-05 17:56:44 +0400

    Feat better user activity snapshot fot cashback algorithms

--- src/main/java/org/project/karto/domain/card/value_objects/UserActivitySnapshot.java
+++ src/main/java/org/project/karto/domain/card/value_objects/UserActivitySnapshot.java
@@ -2,23 +2,26 @@ package org.project.karto.domain.card.value_objects;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.Period;
 import java.util.UUID;
 
 public record UserActivitySnapshot(
         UUID userID,
-        BigDecimal totalAmountSpent,
-        long totalGiftCardsBought,
+        BigDecimal decaySpent,
+        long decayGiftCardsBought,
         LocalDateTime lastTransactionDate,
         int consecutiveActiveDays,
         boolean lastUsageReachedMaximumCashbackRate) {
 
+    public static final Period DECAY = Period.ofDays(21);
+
     public UserActivitySnapshot {
         if (userID == null) throw new IllegalArgumentException("User id cannot be null");
-        if (totalAmountSpent == null) throw new IllegalArgumentException("totalAmountSpent must not be null");
+        if (decaySpent == null) throw new IllegalArgumentException("decaySpent must not be null");
         if (lastTransactionDate == null) throw new IllegalArgumentException("lastTransactionDate must not be null");
-        if (totalAmountSpent.compareTo(BigDecimal.ZERO) < 0)
-            throw new IllegalArgumentException("totalAmountSpent must be non-negative");
-        if (totalGiftCardsBought < 0) throw new IllegalArgumentException("totalGiftCardsBought must be non-negative");
+        if (decaySpent.compareTo(BigDecimal.ZERO) < 0)
+            throw new IllegalArgumentException("decaySpent must be non-negative");
+        if (decayGiftCardsBought < 0) throw new IllegalArgumentException("decayGiftCardsBought must be non-negative");
         if (consecutiveActiveDays < 0) throw new IllegalArgumentException("consecutiveActiveDays must be non-negative");
     }
 
