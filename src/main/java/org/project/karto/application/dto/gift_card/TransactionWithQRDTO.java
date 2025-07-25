1fd096a64bcc0276efeb6d7598bd32afac116f35
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-07-25 20:13:26 +0400

    current state of affairs

---
+++ src/main/java/org/project/karto/application/dto/gift_card/TransactionWithQRDTO.java
@@ -0,0 +1,7 @@
+package org.project.karto.application.dto.gift_card;
+
+public record TransactionWithQRDTO(
+
+) {
+
+}
--- src/main/java/org/project/karto/infrastructure/client/UPPaymentProcessor.java
+++ src/main/java/org/project/karto/infrastructure/client/UPPaymentProcessor.java
@@ -5,7 +5,12 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.mutiny.core.Vertx;
 import io.vertx.mutiny.ext.web.client.WebClient;
 import jakarta.enterprise.context.ApplicationScoped;
+
+import java.net.URI;
+
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.project.karto.application.dto.gift_card.TransactionDTO;
+import org.project.karto.domain.common.containers.Result;
 
 @ApplicationScoped
 public class UPPaymentProcessor {
@@ -21,16 +26,21 @@ public class UPPaymentProcessor {
 
     private String authToken;
 
-    private final Vertx vertx;
-
     private final WebClient webClient;
 
     UPPaymentProcessor(Vertx vertx) {
-        this.vertx = vertx;
         this.webClient = WebClient.create(vertx);
         this.authToken = login()
                 .await()
                 .indefinitely();
+    }
+
+    public Result<URI, Throwable> manualTransaction(TransactionDTO dto) {
+        return null;
+    }
+
+    public Result<URI, Throwable> transaction() {
+        return null; // TODO
     }
 
     private Uni<String> login() {
@@ -38,7 +48,8 @@ public class UPPaymentProcessor {
                 .putHeader("Content-Type", "application/json")
                 .sendJsonObject(loginForm())
                 .onItem().transform(response -> {
-                    if (response.statusCode() == 200) return response.bodyAsString();
+                    if (response.statusCode() == 200)
+                        return response.bodyAsString();
                     throw new IllegalStateException("Cannot login into UP.");
                 });
     }
