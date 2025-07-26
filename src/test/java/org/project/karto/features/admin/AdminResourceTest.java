c3c535c03072ac9ab355007c6e03868ad0bd70df
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-07-27 00:34:18 +0400

    Feat tests for pictures

--- src/test/java/org/project/karto/features/admin/AdminResourceTest.java
+++ src/test/java/org/project/karto/features/admin/AdminResourceTest.java
@@ -7,12 +7,18 @@ import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.core.Response;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.project.karto.application.dto.auth.RegistrationForm;
+import org.project.karto.domain.companies.entities.Company;
+import org.project.karto.domain.companies.repository.CompanyRepository;
 import org.project.karto.infrastructure.security.JWTUtility;
 import org.project.karto.util.DBManagementUtils;
 import org.project.karto.util.PostgresTestResource;
 import org.project.karto.util.TestDataGenerator;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -26,10 +32,17 @@ class AdminResourceTest {
     JWTUtility jwtUtility;
 
     @Inject
+    CompanyRepository repo;
+
+    @Inject
     ObjectMapper mapper;
 
     @Inject
     DBManagementUtils dbManagementUtils;
+
+    static final String ADD_IMAGE = "karto/admin/patner/cards/picture/put";
+
+    static final String GET_IMAGE = "karto/admin/partner/cards/picture";
 
     @Test
     void successfulPartnerRegistration() throws Exception {
@@ -65,5 +78,94 @@ class AdminResourceTest {
                 .patch("karto/admin/ban/user")
                 .then()
                 .statusCode(Response.Status.OK.getStatusCode());
+    }
+
+    @Test
+    void addPicture_shouldReturnAcceptedWhenInputValid() throws IOException {
+        Company company = generateEnableAndSaveCompany();
+        String adminToken = jwtUtility.generateAdministratorToken();
+        byte[] imageData = getImageBytes();
+
+        given()
+                .contentType(ContentType.BINARY)
+                .header("Authorization", "Bearer " + adminToken)
+                .queryParam("companyName", company.companyName().companyName())
+                .body(imageData)
+                .when()
+                .patch(ADD_IMAGE)
+                .then()
+                .statusCode(Response.Status.ACCEPTED.getStatusCode());
+    }
+
+    @Test
+    void addPicture_shouldReturnBadRequestWhenInputStreamNull() {
+        Company company = generateEnableAndSaveCompany();
+        String adminToken = jwtUtility.generateAdministratorToken();
+
+        given()
+                .contentType(ContentType.BINARY)
+                .header("Authorization", "Bearer " + adminToken)
+                .queryParam("companyName", company.companyName().companyName())
+                .when()
+                .patch(ADD_IMAGE)
+                .then()
+                .statusCode(Response.Status.BAD_REQUEST.getStatusCode());
+    }
+
+    @Test
+    void loadPicture_shouldReturnOkWithPictureData() throws IOException {
+        Company company = generateEnableAndSaveCompany();
+        String adminToken = jwtUtility.generateAdministratorToken();
+        String companyName = company.companyName().companyName();
+
+        byte[] imageData = getImageBytes();
+        given()
+                .contentType(ContentType.BINARY)
+                .header("Authorization", "Bearer " + adminToken)
+                .queryParam("companyName", companyName)
+                .body(imageData)
+                .patch(ADD_IMAGE)
+                .then()
+                .statusCode(Response.Status.ACCEPTED.getStatusCode());
+
+        given()
+                .header("Authorization", "Bearer " + adminToken)
+                .queryParam("companyName", companyName)
+                .when()
+                .get(GET_IMAGE)
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode());
+    }
+
+    @Test
+    void loadPicture_shouldReturnNotFoundWhenNoPictureFound() {
+        String adminToken = jwtUtility.generateAdministratorToken();
+        String companyName = "Non-existent Company";
+
+        given()
+                .header("Authorization", "Bearer " + adminToken)
+                .queryParam("companyName", companyName)
+                .when()
+                .get(GET_IMAGE)
+                .then()
+                .statusCode(Response.Status.NOT_FOUND.getStatusCode());
+    }
+
+    private static byte @NotNull [] getImageBytes() throws IOException {
+        try (InputStream is = AdminResourceTest.class
+                .getClassLoader()
+                .getResourceAsStream("democracy.png")) {
+
+            if (is == null) throw new IllegalStateException("Resource not found");
+            return is.readAllBytes();
+        }
+    }
+
+    private Company generateEnableAndSaveCompany() {
+        Company company = TestDataGenerator.generateCompany();
+        company.incrementCounter();
+        company.enable();
+        repo.save(company);
+        return company;
     }
 }
