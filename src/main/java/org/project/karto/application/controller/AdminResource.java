ab3be05f83409e11ee2bf9a869f69f210dae8527
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-07-26 18:14:04 +0400

    Feat endpoint for card pictures

--- src/main/java/org/project/karto/application/controller/AdminResource.java
+++ src/main/java/org/project/karto/application/controller/AdminResource.java
@@ -1,13 +1,21 @@
 package org.project.karto.application.controller;
 
 import jakarta.annotation.security.RolesAllowed;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+
+import java.io.InputStream;
+
 import org.project.karto.application.dto.auth.CompanyRegistrationForm;
 import org.project.karto.application.service.AdminService;
+import static org.project.karto.application.util.RestUtil.responseException;
 
 @Path("/admin")
 @RolesAllowed("ADMIN")
@@ -24,6 +32,26 @@ public class AdminResource {
     public Response partnerRegistration(CompanyRegistrationForm registrationForm) {
         adminService.registerPartner(registrationForm);
         return Response.ok().build();
+    }
+
+    @PATCH
+    @Path("/patner/cards/picture")
+    @Consumes(MediaType.APPLICATION_OCTET_STREAM)
+    public Response addPicture(
+            InputStream inputStream,
+            @QueryParam("companyName") String companyName) {
+
+        if (inputStream == null)
+            throw responseException(Status.BAD_REQUEST, "Picture cannot be null.");
+
+        adminService.putPartnerCardPicture(inputStream, companyName);
+        return Response.accepted().build();
+    }
+
+    @GET
+    @Path("/partner/cards/picture")
+    public Response loadPicture(@QueryParam("companyName") String companyName) {
+        return Response.ok(adminService.loadProfilePicture(companyName)).build();
     }
 
     @PATCH
--- src/main/java/org/project/karto/application/service/AdminService.java
+++ src/main/java/org/project/karto/application/service/AdminService.java
@@ -3,6 +3,8 @@ package org.project.karto.application.service;
 import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.project.karto.application.dto.auth.CompanyRegistrationForm;
 import org.project.karto.application.dto.auth.Token;
@@ -18,11 +20,15 @@ import org.project.karto.domain.companies.value_objects.CompanyName;
 import org.project.karto.domain.companies.value_objects.RegistrationNumber;
 import org.project.karto.domain.user.entities.User;
 import org.project.karto.domain.user.repository.UserRepository;
+import org.project.karto.domain.user.values_objects.PictureOfCards;
 import org.project.karto.infrastructure.communication.PhoneInteractionService;
+import org.project.karto.infrastructure.files.CardPicturesRepository;
+import org.project.karto.infrastructure.files.StreamUtils;
 import org.project.karto.infrastructure.security.HOTPGenerator;
 import org.project.karto.infrastructure.security.JWTUtility;
 import org.project.karto.infrastructure.security.PasswordEncoder;
 
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
@@ -44,15 +50,24 @@ public class AdminService {
 
     private final CompanyRepository companyRepository;
 
+    private final CardPicturesRepository picturesRepository;
+
     private final PhoneInteractionService phoneInteractionService;
 
     private final PartnerVerificationOTPRepository otpRepository;
 
-    AdminService(JWTUtility jwtUtility, PasswordEncoder passwordEncoder, UserRepository userRepository,
-                 CompanyRepository companyRepository, PhoneInteractionService phoneInteractionService,
-                 PartnerVerificationOTPRepository otpRepository) {
+    AdminService(
+            JWTUtility jwtUtility,
+            PasswordEncoder passwordEncoder,
+            UserRepository userRepository,
+            CompanyRepository companyRepository,
+            CardPicturesRepository picturesRepository,
+            PhoneInteractionService phoneInteractionService,
+            PartnerVerificationOTPRepository otpRepository) {
+
         this.jwtUtility = jwtUtility;
         this.userRepository = userRepository;
+        this.picturesRepository = picturesRepository;
         this.phoneInteractionService = phoneInteractionService;
         this.hotpGenerator = new HOTPGenerator();
         this.passwordEncoder = passwordEncoder;
@@ -103,14 +118,34 @@ public class AdminService {
                 phone,
                 encodedPassword,
                 HOTPGenerator.generateSecretKey(),
-                CardUsageLimitations.of(registrationForm.cardExpirationDays(), registrationForm.cardMaxUsageCount())
-        );
+                CardUsageLimitations.of(registrationForm.cardExpirationDays(), registrationForm.cardMaxUsageCount()));
 
         companyRepository.save(company)
                 .orElseThrow(() -> responseException(Response.Status.INTERNAL_SERVER_ERROR,
                         "Unable to save company. Please try again later."));
 
         generateAndResendPartnerOTP(company);
+    }
+
+    public void putPartnerCardPicture(InputStream inputStream, String companyName) {
+        Company company = companyRepository.findBy(new CompanyName(companyName))
+                .orElseThrow(() -> responseException(Status.NOT_FOUND, "This partner is not found."));
+
+        final byte[] pictureBytes = StreamUtils.toByteArray(inputStream)
+                .orElseThrow(() -> responseException(Status.BAD_REQUEST, "Invalid picture."));
+
+        final PictureOfCards picture = PictureOfCards.of(pictureBytes, company);
+        company.addPicture(picture);
+
+        picturesRepository.put(company);
+    }
+
+    public PictureOfCards loadProfilePicture(String companyName) {
+        Company company = companyRepository.findBy(new CompanyName(companyName))
+                .orElseThrow(() -> responseException(Status.NOT_FOUND, "This partner is not found."));
+
+        return picturesRepository.load(PictureOfCards.profilePicturePath(company.id().toString()))
+                .orElseThrow(() -> responseException(Status.NOT_FOUND, "Picture not found."));
     }
 
     public void banUser(String receivedPhone) {
@@ -124,7 +159,8 @@ public class AdminService {
 
     private void generateAndResendPartnerOTP(Company company) {
         PartnerVerificationOTP otp = PartnerVerificationOTP
-                .of(company, hotpGenerator.generateHOTP(company.keyAndCounter().key(), company.keyAndCounter().counter()));
+                .of(company,
+                        hotpGenerator.generateHOTP(company.keyAndCounter().key(), company.keyAndCounter().counter()));
 
         otpRepository.save(otp)
                 .orElseThrow(() -> responseException(Response.Status.INTERNAL_SERVER_ERROR,
