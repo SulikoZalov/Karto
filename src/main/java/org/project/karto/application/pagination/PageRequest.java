1ffa981b0002c9c2d7db6bd77498711bd233abd6
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-07-27 16:30:26 +0400

    Fix page request

--- src/main/java/org/project/karto/application/pagination/PageRequest.java
+++ src/main/java/org/project/karto/application/pagination/PageRequest.java
@@ -8,5 +8,28 @@ public record PageRequest(int limit, int offset) {
             throw new IllegalDomainArgumentException("Offset cannot be negative");
         if (limit <= 0)
             throw new IllegalDomainArgumentException("Limit must be positive");
+
+        limit = buildLimit(limit);
+        offset = buildOffSet(limit, offset);
+    }
+
+    static int buildLimit(Integer pageSize) {
+        int limit;
+        if (pageSize > 0 && pageSize <= 25) {
+            limit = pageSize;
+        } else {
+            limit = 10;
+        }
+        return limit;
+    }
+
+    static int buildOffSet(Integer limit, Integer pageNumber) {
+        int offSet;
+        if (limit > 0 && pageNumber > 0) {
+            offSet = (pageNumber - 1) * limit;
+        } else {
+            offSet = 0;
+        }
+        return offSet;
     }
 }
