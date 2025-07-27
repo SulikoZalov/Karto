3fe0fa376a3e84c968e9a4ae1eb7225e387973ce
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-07-27 18:00:14 +0400

    Feat better global exception mapper

--- src/main/java/org/project/karto/infrastructure/exceptions_handler/ExceptionHandler.java
+++ src/main/java/org/project/karto/infrastructure/exceptions_handler/ExceptionHandler.java
@@ -1,29 +1,55 @@
 package org.project.karto.infrastructure.exceptions_handler;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hadzhy.jetquerious.exceptions.NotFoundException;
 import io.quarkus.logging.Log;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
+
+import org.project.karto.application.dto.common.ErrorMessage;
 import org.project.karto.domain.common.exceptions.DomainException;
+import org.project.karto.domain.user.exceptions.BannedUserException;
 
 @Provider
 public class ExceptionHandler implements ExceptionMapper<Throwable> {
 
+  static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
   @Override
   public Response toResponse(Throwable e) {
     Log.error("Global error handler: ", e);
-    if (e instanceof DomainException) return Response
-            .status(Response.Status.BAD_REQUEST)
-            .entity(e.getMessage())
-            .build();
+    if (e instanceof BannedUserException)
+      return Response
+          .status(Status.FORBIDDEN)
+          .entity(errorMessage(e.getMessage()))
+          .build();
 
-    if (e instanceof NotFoundException) return Response
-            .status(Response.Status.NOT_FOUND)
-            .build();
+    if (e instanceof DomainException)
+      return Response
+          .status(Response.Status.BAD_REQUEST)
+          .entity(errorMessage(e.getMessage()))
+          .build();
+
+    if (e instanceof NotFoundException)
+      return Response
+          .status(Response.Status.NOT_FOUND)
+          .build();
 
     return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-            .entity("Unexpected error occurred. Please contact support.")
-            .build();
+        .entity(errorMessage("Unexpected error occurred. Please contact support."))
+        .build();
+  }
+
+  private static String errorMessage(String message) {
+    try {
+    String result =OBJECT_MAPPER.writeValueAsString(new ErrorMessage(message));
+      return result;
+    } catch (JsonProcessingException e) {
+      Log.error("Unexpected error. Cannot process json in exception mapper." e);
+      return null;
+    }
   }
 }
