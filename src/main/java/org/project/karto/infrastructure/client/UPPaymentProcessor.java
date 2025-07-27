7525a740ab65c341006e864fe74b221f4bebae04
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-07-28 03:26:43 +0400

    Feat client function for initialization of manual payment

--- src/main/java/org/project/karto/infrastructure/client/UPPaymentProcessor.java
+++ src/main/java/org/project/karto/infrastructure/client/UPPaymentProcessor.java
@@ -3,14 +3,19 @@ package org.project.karto.infrastructure.client;
 import io.smallrye.mutiny.Uni;
 import io.vertx.core.json.JsonObject;
 import io.vertx.mutiny.core.Vertx;
+import io.vertx.mutiny.core.buffer.Buffer;
 import io.vertx.mutiny.ext.web.client.WebClient;
 import jakarta.enterprise.context.ApplicationScoped;
 
 import java.net.URI;
+import java.util.Base64;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.project.karto.application.dto.gift_card.TransactionDTO;
 import org.project.karto.domain.common.containers.Result;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ApplicationScoped
 public class UPPaymentProcessor {
@@ -24,9 +29,14 @@ public class UPPaymentProcessor {
     @ConfigProperty(name = "up.auth.password")
     String registrationPassword;
 
+    @ConfigProperty(name = "up.checkout")
+    String checkoutURL;
+
     private String authToken;
 
     private final WebClient webClient;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     UPPaymentProcessor(Vertx vertx) {
         this.webClient = WebClient.create(vertx);
@@ -35,12 +45,23 @@ public class UPPaymentProcessor {
                 .indefinitely();
     }
 
-    public Result<URI, Throwable> manualTransaction(TransactionDTO dto) {
-        return null;
-    }
+    public Result<URI, Throwable> initiateManualPayment(TransactionDTO dto) {
+        try {
+            var response = webClient.postAbs(checkoutURL)
+                    .putHeader("Content-Type", "application/json")
+                    .putHeader("Authorization", "Bearer " + authToken)
+                    .sendBuffer(Buffer.buffer(encode64(dto)))
+                    .await()
+                    .indefinitely();
 
-    public Result<URI, Throwable> transaction() {
-        return null; // TODO
+            if (response.statusCode() != 200)
+                return Result.failure(new IllegalStateException("Can`t initialize transaction via payment processor."));
+
+            URI paymentURI = URI.create(response.bodyAsJsonObject().getString("url"));
+            return Result.success(paymentURI);
+        } catch (Throwable t) {
+            return Result.failure(t);
+        }
     }
 
     private Uni<String> login() {
@@ -52,6 +73,10 @@ public class UPPaymentProcessor {
                         return response.bodyAsString();
                     throw new IllegalStateException("Cannot login into UP.");
                 });
+    }
+
+    private String encode64(TransactionDTO dto) throws JsonProcessingException {
+        return Base64.getEncoder().encodeToString(objectMapper.writeValueAsString(dto).getBytes());
     }
 
     private JsonObject loginForm() {
