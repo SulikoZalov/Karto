1efb2072924120a3ce91dde9acaa49eaedef2c55
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-07-27 15:27:50 +0400

    Feat better card form

--- src/main/java/org/project/karto/application/dto/gift_card/CardForm.java
+++ src/main/java/org/project/karto/application/dto/gift_card/CardForm.java
@@ -1,7 +1,15 @@
 package org.project.karto.application.dto.gift_card;
 
+import java.math.BigDecimal;
+
 import org.project.karto.domain.card.enumerations.GiftCardRecipientType;
 import org.project.karto.domain.card.enumerations.GiftCardType;
 import org.project.karto.domain.card.value_objects.StoreID;
 
-public record CardForm(GiftCardRecipientType recipientType, GiftCardType cardType, StoreID store) {}
+public record CardForm(
+    GiftCardRecipientType recipientType,
+    GiftCardType cardType,
+    StoreID store,
+    BigDecimal amount,
+    String language) {
+}
