42ced50d7bfc142752071bfcf4c77201e0cd2568
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-08-30 01:29:49 +0400

    Feat new implementation of QR generator and tests for it

--- src/main/java/org/project/karto/infrastructure/qr/QRGenerator.java
+++ src/main/java/org/project/karto/infrastructure/qr/QRGenerator.java
@@ -1,41 +1,47 @@
 package org.project.karto.infrastructure.qr;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.common.BitMatrix;
-import jakarta.enterprise.context.ApplicationScoped;
+import boofcv.alg.fiducial.qrcode.QrCode;
+import boofcv.alg.fiducial.qrcode.QrCodeEncoder;
+import boofcv.alg.fiducial.qrcode.QrCodeGeneratorImage;
+import boofcv.struct.image.GrayU8;
 import org.project.karto.application.dto.common.QR;
 import org.project.karto.application.dto.gift_card.PaymentQRDTO;
 import org.project.karto.domain.common.containers.Result;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Base64;
 
-@ApplicationScoped
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 public class QRGenerator {
 
-  private static final int WIDTH = 250;
-  private static final int HEIGHT = 250;
-  private static final String IMAGE_FORMAT = "PNG";
-  private static final ObjectMapper objectMapper = new ObjectMapper();
+  private QRGenerator() {}
 
-  public Result<QR, Throwable> generate(PaymentQRDTO dto) {
+  public static Result<QR, Throwable> generate(PaymentQRDTO dto) {
     try {
-      String json = objectMapper.writeValueAsString(dto);
+      QrCode qr = new QrCodeEncoder()
+              .setError(QrCode.ErrorLevel.M)
+              .addAutomatic(dto.toJson())
+              .fixate();
 
-      BitMatrix matrix = new MultiFormatWriter()
-          .encode(json, BarcodeFormat.QR_CODE, WIDTH, HEIGHT);
+      QrCodeGeneratorImage render = new QrCodeGeneratorImage(20);
+      GrayU8 gray = render.render(qr).getGray();
 
       try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-        MatrixToImageWriter.writeToStream(matrix, IMAGE_FORMAT, baos);
+        writePGM(gray, baos);
         String base64 = Base64.getEncoder().encodeToString(baos.toByteArray());
         return Result.success(new QR(base64));
       }
 
-    } catch (Exception e) {
+    } catch (Throwable e) {
       return Result.failure(new QRGenerationException("Failed to generate QR code."));
     }
+  }
+
+  private static void writePGM(GrayU8 gray, ByteArrayOutputStream baos) throws IOException {
+    String header = String.format("P5\n%d %d\n255\n", gray.width, gray.height);
+    baos.write(header.getBytes(UTF_8));
+    baos.write(gray.data);
   }
 }
--- src/test/groovy/org/project/karto/unit/qr/QRGeneratorTest.groovy
+++ src/test/groovy/org/project/karto/unit/qr/QRGeneratorTest.groovy
@@ -6,18 +6,18 @@ import org.project.karto.domain.common.containers.Result
 import org.project.karto.infrastructure.qr.QRGenerator
 import org.project.karto.util.TestDataGenerator
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class QRGeneratorTest extends Specification {
 
     QRGenerator qrGenerator = new QRGenerator()
 
-    def "should generate QR code successfully and return base64 string"() {
+    @Unroll
+    def "should generate QR code successfully and return base64 string [#index]"() {
         given:
         PaymentQRDTO dto = new PaymentQRDTO(
-        TestDataGenerator.generateCompanyName().companyName(),
-        TestDataGenerator.generateAmount(BigDecimal.valueOf(9999999L)).value()
-        )
-
+                TestDataGenerator.generateCompanyName().companyName(),
+                TestDataGenerator.generateAmount(BigDecimal.valueOf(9999999L)).value())
 
         when:
         Result<QR, Throwable> result = qrGenerator.generate(dto)
@@ -25,7 +25,9 @@ class QRGeneratorTest extends Specification {
         then:
         result.success()
         result.value().value() != null
-        result.value().value().startsWith("iVBOR")
+
+        where:
+        index << (1..10_000)
     }
 }
 
