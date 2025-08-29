7027dabe108c2ae78c3cf0d8552a89418de49332
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-08-29 19:40:31 +0400

    current state of affairs

--- src/main/java/org/project/karto/application/service/GiftCardsService.java
+++ src/main/java/org/project/karto/application/service/GiftCardsService.java
@@ -2,23 +2,20 @@ package org.project.karto.application.service;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.core.Response.Status;
-
-import java.util.List;
-
 import org.project.karto.application.dto.gift_card.CardDTO;
 import org.project.karto.application.dto.gift_card.CardForm;
 import org.project.karto.application.dto.gift_card.SpendRequest;
 import org.project.karto.application.pagination.PageRequest;
 import org.project.karto.domain.card.repositories.CardVerificationOTPRepository;
 import org.project.karto.domain.card.repositories.GiftCardRepository;
-import org.project.karto.domain.common.value_objects.Amount;
 import org.project.karto.domain.common.value_objects.Email;
-import org.project.karto.domain.common.value_objects.Language;
-import org.project.karto.domain.user.entities.User;
 import org.project.karto.domain.user.repository.UserRepository;
+import org.project.karto.infrastructure.client.UPPaymentProcessor;
 
-import static org.project.karto.domain.common.util.Utils.required;
+import java.util.List;
+
 import static org.project.karto.application.util.RestUtil.responseException;
+import static org.project.karto.domain.common.util.Utils.required;
 
 @ApplicationScoped
 public class GiftCardsService {
@@ -27,23 +24,24 @@ public class GiftCardsService {
 
     private final GiftCardRepository giftCardRepository;
 
+    private final UPPaymentProcessor paymentProcessor;
+
     private final CardVerificationOTPRepository cardVerificationRepository;
 
     GiftCardsService(
             UserRepository userRepository,
+            UPPaymentProcessor paymentProcessor,
             GiftCardRepository giftCardRepository,
             CardVerificationOTPRepository cardVerificationRepository) {
+
         this.userRepository = userRepository;
+        this.paymentProcessor = paymentProcessor;
         this.giftCardRepository = giftCardRepository;
         this.cardVerificationRepository = cardVerificationRepository;
     }
 
     public void create(CardForm cardForm, Email email) {
-        if (cardForm == null)
-            throw responseException(Status.BAD_REQUEST, "Card form is required.");
-
-        User user = userRepository.findBy(email).orElseThrow();
-
+        // TODO
     }
 
     public void verify(String otp, Email email) {
